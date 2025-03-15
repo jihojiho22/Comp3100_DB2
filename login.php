950311fb71fbe731539c8f1f5953c9b992e4c505
Jiho Lee <61943484+jihojiho22@users.noreply.github.com>
2025-03-15 18:53:39 -0400

    .

--- login.php
+++ login.php
@@ -4,7 +4,6 @@ require_once 'config&functions.php';
 
 // Check if the user is already logged in
 if (isset($_SESSION['user_id'])) {
-    // Redirect to student.php or another page (e.g., dashboard)
     header('Location: student.php?page=home');
     exit;
 }
@@ -13,24 +12,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
 
-    // Verify user credentials
     list($success, $user_data) = verify_login($email, $password);
 
     if ($success) {
-        // Set session variables
         $_SESSION['user_id'] = $email;
         $_SESSION['user_type'] = $user_data['type'];
         
-        // Redirect to the home page
         header('Location: student.php?page=home');
         exit;
     } else {
         $_SESSION['login_error'] = "Invalid email or password.";
-        header('Location: index.html'); // Redirect to login page with error
+        header('Location: index.html');
         exit;
     }
 } else {
-    // If accessed directly without POST, redirect to login page
     header('Location: index.html');
     exit;
 }
--- logout.php
+++ logout.php
@@ -4,7 +4,6 @@ session_start();
 // Unset all session variables
 $_SESSION = [];
 
-// Destroy the session
 session_destroy();
 
 // Redirect to login page
